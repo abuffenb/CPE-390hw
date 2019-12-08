9596000bf5f4e1e565243f8fc850b10fde428b1e
Allison Buffenbarger <abuffenb@stevens.edu>
2019-12-08 15:23:09 -0500

    updated eratos

---
+++ eratos/eratos.s
@@ -0,0 +1,147 @@
+	@Author: Allison Buffenbarger
+	@I pledge my honor that I have abided by the Stevens Honor System.
+	@I collaborated with Sarah Bertussi. -Allison Buffenbarger
+
+	.global _Z3eraPjj
+
+_Z3eraPjj:
+
+	push {r3-r11}
+	
+	@r0=array starts here
+	@r1=# of numbers
+	@r2=square root of # of numbers
+	@r11 = counter
+
+	mov r11, #1
+
+	@task 1:
+	@for(int i=2, i<sqrt(n), i++) {
+	@for(int j=i*i, j<(n), j=j+i) {
+	@prime[j]=false }}
+
+	@task: assume all numbers = prime
+	@figure out how many words needed = (R+31)/32
+	@r3=0xFFFFFFFF
+	@r4=(r1+31)/32=number of words
+
+	ldr r3, =0xFFFFFFFF	@r3=0xFFFFFFFF
+	add r4, r1, #31		@r4=r1+31
+	lsr r4, #5		@r4=(r1+31)/32
+
+	
+	@task 2: find a maximum offset
+	@max offset = 4*number of numbers - 4 = 4*r4-4
+
+	lsl r4, #2
+	sub r4, r4, #4
+
+	@r4 = maximum offset
+	@r5 = current offset
+
+	mov r5, #0
+
+.makeprime:
+
+	str r3, [r0, r5]	@set number at current offset to 0xFFFFFFFF
+	add r5, r5, #4	@increment offset by 4
+	cmp r5, r4	@compare current offset and maximum offset
+	ble .makeprime
+
+	@Summary:
+	@r0=array
+	@r1=# of numbers
+	@r2=sqrt(# of numbers)
+	@r3=0xFFFFFFFF
+	@r4=max offset = 4*number of words - 4
+	@r5 =max offset (used to be current offset but changed b/c .makeprime)
+
+
+	@task 3:
+	@for(int i=2, i<sqrt(n), i++) {
+	@for(int j=i*i, j<(n), j=j+i) {
+	@Prime[j]=false}}
+
+
+	@r1=n
+	@r2=sqrt(n)
+	@r3=i
+	@r4=j
+
+	mov r3, #2	@r3=2
+
+.outer:
+
+	mul r4, r3, r3	@reset j to i^2 when finish outerloop
+
+.inner:
+
+	@need to point to word we're on
+	@word = number/32 = number LSR 5 = j/32
+	@lsr r5, r4, #5  >>>> @r5=word we're on
+
+	@to find which bit in word that we're on
+	@j mod 32 = bit number
+	@r4 AND 31 = r4 mod 32
+
+	and r6, r4, #31
+
+	@Summary:
+	@r5=r4>>5
+	@r6=r4 AND 31
+
+	@r5=word
+	@r6=position of bit
+	@r7=offset = r5*4
+
+	lsl r7, r5, #2
+
+	@r8=will clear bits
+
+	mov r8, #1
+	lsl r8, r6
+	mvn r8, r8
+
+	@task:
+	@get word from specific offset and place in r10 then clear bit
+
+	ldr r9, [r0, r7]
+	mov r10, r9
+	and r9, r8
+	str r9, [r0, r7]
+
+	@Summary:
+	@r1=n
+	@r2=sqrt(n)
+	@r3=i
+	@r4=j
+	@r5=word=j/32=r4>>5
+	@r6=bit position= j mod 32 = r4 and 32
+	@r7 =offset=r5*4=word*4
+	@r8=11101111
+	@r9=modified word
+	@r10=original word
+
+
+	b .counter 	@branch to counter
+	.return:		@return from counter
+
+	add r4, r3
+	cmp r4, r1 @j < n
+	ble .inner
+
+	add r3, r3, #1
+	cmp r3, r2	@i < sqrt(n)
+	ble .outer
+	sub r0, r1, r11	@# of bits - # of bits changed = number of bits remaining
+
+	pop {r3-r11}
+	bx lr
+
+.counter:	@will show how many bits changed if og word and mod word are different
+
+	cmp r9, r10
+	addne r11, r11, #1
+	b .return
+	
+	
---
+++ eratos/main.s
@@ -0,0 +1,157 @@
+	.file	"main.cc"
+	.text
+	.section .rdata,"dr"
+_ZStL19piecewise_construct:
+	.space 1
+.lcomm _ZStL8__ioinit,1,1
+	.def	__main;	.scl	2;	.type	32;	.endef
+.LC0:
+	.ascii "Primes up to \0"
+.LC1:
+	.ascii " = \0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1581:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$64, %rsp
+	.seh_stackalloc	64
+	.seh_endprologue
+	call	__main
+	movl	$1000000000, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addl	$31, %eax
+	shrl	$5, %eax
+	movl	%eax, %eax
+	movabsq	$2305843009213693950, %rdx
+	cmpq	%rdx, %rax
+	ja	.L2
+	salq	$2, %rax
+	movq	%rax, %rcx
+	call	_Znay
+	movq	%rax, -16(%rbp)
+	movl	-4(%rbp), %edx
+	movq	-16(%rbp), %rax
+	movq	%rax, %rcx
+	call	_Z3eraPjj
+	movl	%eax, -20(%rbp)
+	leaq	.LC0(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEj
+	leaq	.LC1(%rip), %rdx
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-20(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEj
+	movl	$10, %edx
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	cmpq	$0, -16(%rbp)
+	je	.L4
+	jmp	.L6
+.L2:
+	call	__cxa_throw_bad_array_new_length
+.L6:
+	movq	-16(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZdaPv
+.L4:
+	movl	$0, %eax
+	addq	$64, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB2068:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitD1Ev
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_Z41__static_initialization_and_destruction_0ii
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2067:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	cmpl	$1, 16(%rbp)
+	jne	.L10
+	cmpl	$65535, 24(%rbp)
+	jne	.L10
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rcx
+	call	atexit
+.L10:
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I_main
+_GLOBAL__sub_I_main:
+.LFB2069:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	$65535, %edx
+	movl	$1, %ecx
+	call	_Z41__static_initialization_and_destruction_0ii
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 9.2.0"
+	.def	_Znay;	.scl	2;	.type	32;	.endef
+	.def	_Z3eraPjj;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEj;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c;	.scl	2;	.type	32;	.endef
+	.def	__cxa_throw_bad_array_new_length;	.scl	2;	.type	32;	.endef
+	.def	_ZdaPv;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	atexit;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
